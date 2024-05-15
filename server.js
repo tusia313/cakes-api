cd2c9c4baa77a96282f02fb85b949b656d9a947c
Marta Łoskot <loskotmarta@gmail.com>
2024-05-15 20:04:42 +0200

    Use json-server module in server.js

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 3000
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router('db.json')
+// const middlewares = jsonServer.defaults()
+// const port = process.env.PORT || 3000
 
-server.use(middlewares)
-server.use(router)
+// server.use(middlewares)
+// server.use(router)
 
-server.listen()
+// server.listen()
+const jsonServer = require('json-server');
+const db = require('./db.json');
+const PORT = process.env.PORT || 3000;
+
+const server = jsonServer.create();
+const router = jsonServer.router(db);
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
