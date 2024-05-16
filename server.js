6baa23894d908ea11d98c1aa1abed6e34a972e02
Marta Łoskot <loskotmarta@gmail.com>
2024-05-16 13:28:16 +0200

    Move json-server to production dependencies

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 3000
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router('db.json')
+// const middlewares = jsonServer.defaults()
+// const port = process.env.PORT || 3000
 
-server.use(middlewares)
-server.use(router)
+// server.use(middlewares)
+// server.use(router)
+// server.listen()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.listen()
+const PORT = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
+
+
+
+
